cab1e6dbe6562460668912317ca86f1712cdea44
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-04-09 18:40:38 +0200

    Configure hyprpaper

--- applications/home-manager/hypr/hyprpaper.nix
+++ applications/home-manager/hypr/hyprpaper.nix
@@ -1,11 +1,25 @@
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  wallpaper = pkgs.fetchurl {
+    url = "https://cdn.wallpapersafari.com/10/81/0VQDfp.jpg";
+    sha256 = "sha256-hpY2miVwRcBfNlpEoP6NsmpOJaiwB01AXmyPTISjN8A=";
+  };
+in
+{
+  home.file."${config.xdg.dataHome}/wallpapers/main.jpg".source = wallpaper;
+
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = ../../../assets/thefinals_logo.png;
+      preload = [ "${config.xdg.dataHome}/wallpapers/main.jpg" ];
       wallpaper = {
         monitor = "";
-        path = ../../../assets/thefinals_logo.png;
+        path = [ "${config.xdg.dataHome}/wallpapers/main.jpg" ];
       };
     };
   };
