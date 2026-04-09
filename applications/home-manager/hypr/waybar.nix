d51b85dc5977475197f72a0299ca822b72056b9e
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-04-09 16:18:49 +0200

    Add example style from mynixos.com

--- applications/home-manager/hypr/waybar.nix
+++ applications/home-manager/hypr/waybar.nix
@@ -25,6 +25,20 @@
           };
         };
       };
+      style = ''
+        * {
+          border: none;
+          border-radius: 0;
+          font-family: Source Code Pro;
+        }
+        window#waybar {
+          background: #16191C;
+          color: #AAB2BF;
+        }
+        #workspaces button {
+          padding: 0 5px;
+        }
+      '';
     };
   };
   wayland.windowManager.hyprland = {
