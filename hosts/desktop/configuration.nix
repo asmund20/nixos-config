3b1518478b14405b4e8db785c8a361c11f6394b5
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-04-09 11:28:00 +0200

    Change path to windows boot.efi

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -22,7 +22,7 @@
     /Windows
     comment: Windows
     protocol: efi
-    image_path: \EFI\Microsoft\Boot\bootmgfw.efi
+    image_path: \EFI\BOOT\BOOTX64.EFI
     part_uuid: d6c351ce-c388-42eb-bdcc-c70bef8e10bf
   '';
 
