4d4ac7eb4b1bedb1c331c994f7122800ae0b57fe
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-04-09 10:57:34 +0200

    Add windows to limine

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -16,6 +16,12 @@
   # Bootloader.
   boot.loader.limine.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.limine.extraConfig = ''
+    :Windows
+        COMMENT=Windows
+        PROTOCOL=chainload
+        PART_UUID=d6c351ce-c388-42eb-bdcc-c70bef8e10bf
+  '';
 
   networking.hostName = "nixos"; # Define your hostname.
 
