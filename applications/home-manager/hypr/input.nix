9c1222f0b46645501e0789aa684b2d710304177c
Åsmund <asmund.sl@proton.me>
2026-04-10 17:06:52 +0200

    Do some touchpad stuff

--- applications/home-manager/hypr/input.nix
+++ applications/home-manager/hypr/input.nix
@@ -6,6 +6,12 @@
         repeat_rate = 40;
         repeat_delay = 600;
         sensitivity = -0.7;
+        touchpad = {
+          disable_while_typing = true;
+          natural_scroll = true;
+          scroll_factor = 0.5;
+
+        };
       };
       cursor = {
         hide_on_key_press = true;
