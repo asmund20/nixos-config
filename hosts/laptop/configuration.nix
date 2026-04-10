110baf894011e5196786f9b4ac1e121e789f54d8
Åsmund <asmund.sl@proton.me>
2026-04-10 14:44:04 +0200

    Use limine again?

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -16,7 +16,7 @@
   # Bootloader.
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.limine.enable = true;
 
   networking.hostName = "nixos"; # Define your hostname.
 
