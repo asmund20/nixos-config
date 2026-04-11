deab96a8fcd704c6c98895831fa7ce321576d5d8
Åsmund <asmund.sl@proton.me>
2026-04-11 15:00:43 +0200

    Add idle-dimming of display and comment out sleep stuff that did not work

--- applications/home-manager/hypr/hypridle.nix
+++ applications/home-manager/hypr/hypridle.nix
@@ -10,14 +10,19 @@
 
       listener = [
         {
-          timeout = 900;
-          on-timeout = "hyprlock";
+          timeout = 3 * 60;
+          on-timeout = "brightnessctl -s && brightnessctl s 5%";
+          on-resume = "brightnessctl -r";
         }
         {
-          timeout = 1200;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
+          timeout = 5 * 60;
+          on-timeout = "hyprlock";
         }
+        # {
+        #   timeout = 6*60;
+        #   on-timeout = "hyprctl dispatch dpms off";
+        #   on-resume = "hyprctl dispatch dpms on";
+        # }
       ];
     };
   };
