6ffd4b321cab0fcfb1e6e40607f21686b62de634
Åsmund <asmund.sl@proton.me>
2026-04-16 12:51:51 +0200

    Add todo for better sensitivity handling

--- applications/home-manager/hypr/input.nix
+++ applications/home-manager/hypr/input.nix
@@ -6,6 +6,8 @@
         kb_variant = "nodeadkeys";
         repeat_rate = 40;
         repeat_delay = 600;
+        # TODO: do the sensitivity in extra-config for the gaming mouse,
+        # instead of setting all other mice to sensitivity zero
         sensitivity = -0.7;
         touchpad = {
           disable_while_typing = true;
