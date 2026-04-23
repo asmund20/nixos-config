7895f51c52a5a0d2b8879c75f20883452c1de308
Åsmund <asmund.sl@proton.me>
2026-04-23 22:59:14 +0200

    Remove irrelevant treesitter stuff, add haskell ftplugin for tab management

--- applications/home-manager/nvim/nvim/after/plugin/treesitter.lua
+++ applications/home-manager/nvim/nvim/after/plugin/treesitter.lua
@@ -1,13 +1,6 @@
 require "nvim-treesitter".setup {
-    ensure_installed = { "python", "lua", "typst", "markdown_inline", "markdown" },
-
-    sync_install = false,
-
-    auto_install = true,
-
     highlight = {
         enable = true,
-
         additional_vim_regex_highlighting = false,
     }
 }
