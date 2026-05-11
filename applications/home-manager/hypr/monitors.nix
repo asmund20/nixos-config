75b33d45bbc990e3043856f4995939c2dfe771a3
Åsmund <asmund.sl@proton.me>
2026-05-11 10:09:23 +0200

    Add monitor stuff

--- applications/home-manager/hypr/monitors.nix
+++ applications/home-manager/hypr/monitors.nix
@@ -1,6 +1,7 @@
 {
   wayland.windowManager.hyprland.extraConfig = ''
     monitor = , preferred, auto, 1
+    monitor = eDP-1, preferred, auto-down, 1
     monitor = HDMI-A-1, preferred, auto-up, 1
   '';
 }
