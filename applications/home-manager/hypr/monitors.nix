e91ea671463df5a742afeb51d3b72b4097b396c7
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-07-22 22:31:15 +0200

    Revert "Try rotating dell display type stuff"
    This reverts commit 5eeff43e5d576e90147344dbf26037537e67b290.

--- applications/home-manager/hypr/monitors.nix
+++ applications/home-manager/hypr/monitors.nix
@@ -3,6 +3,5 @@
     monitor = , preferred, auto, 1
     monitor = eDP-1, preferred, auto-down, 1
     monitor = HDMI-A-1, preferred, auto-up, 1
-    monitor = Dell Inc. DELL U2717D J0XYN95AA06L, preferred, auto-left, 1, transform, 1
   '';
 }
