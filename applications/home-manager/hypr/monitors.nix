39d5b604b5dfd68270068aed16789acd20c5f786
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-07-22 22:26:20 +0200

    Try removing hdmi config

--- applications/home-manager/hypr/monitors.nix
+++ applications/home-manager/hypr/monitors.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland.extraConfig = ''
     monitor = , preferred, auto, 1
     monitor = eDP-1, preferred, auto-down, 1
-    monitor = HDMI-A-1, preferred, auto-up, 1
     monitor = "Dell Inc. DELL U2717D J0XYN95AA06L", preferred, auto-left, 1, transform, 1
   '';
+  # monitor = HDMI-A-1, preferred, auto-up, 1
 }
