f180443425ed36ff35a66bd03b82a8395780ad5c
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-08-23 20:38:30 +0200

    Setup monitors at home office

--- applications/home-manager/hypr/monitors.nix
+++ applications/home-manager/hypr/monitors.nix
@@ -3,5 +3,7 @@
     monitor = , preferred, auto, 1
     monitor = eDP-1, preferred, auto-down, 1
     monitor = HDMI-A-1, preferred, auto-up, 1
+    monitor = desc:ASUSTek COMPUTER INC ASUS VG249 0x000375FB, 1920x1080, 1440x800, 1
+    monitor = desc:Dell Inc. DELL U2717D J0XYN95AA06L, 2560x1440, 0x0, 1, transform, 1
   '';
 }
