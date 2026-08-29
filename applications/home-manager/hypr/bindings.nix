b9e71deba348979ea66e7d97bcbdb4596ec78aae
Åsmund <asmund.sl@proton.me>
2026-08-29 22:12:49 +0200

    Make subset of bindings work

--- applications/home-manager/hypr/bindings.nix
+++ applications/home-manager/hypr/bindings.nix
@@ -2,17 +2,54 @@
 
 let
   mkLua = lib.generators.mkLuaInline;
+  mod = a: b: a - (b * (a / b));
+  directions = {
+    left = "h";
+    right = "l";
+    up = "k";
+    down = "j";
+  };
+  staticBinds = [
+      {
+        _args = [
+          (mkLua "mod .. \" + T\"")
+          (mkLua "hl.dsp.exec_cmd(\"ghostty\")")
+        ];
+      }
+      {
+        _args = [
+          (mkLua "mod .. \" + space\"")
+          (mkLua "hl.dsp.exec_cmd(\"hyprlauncher\")")
+        ];
+      }
+      {
+        _args = [
+          (mkLua "mod .. \" + C\"")
+          (mkLua "hl.dsp.exec_cmd(\"hyprlock\")")
+        ];
+      }
+      {
+        _args = [
+          (mkLua "mod .. \" + B\"")
+          (mkLua "hl.dsp.exec_cmd(\"zen\")")
+        ];
+      }
+      {
+        _args = [
+          (mkLua "mod .. \" + W\"")
+          (mkLua "hl.dsp.window.close()")
+        ];
+      }
+    ];
 
 in
 {
   wayland.windowManager.hyprland.settings = {
-    bind = [
-      {
-        _args = [
-          "SUPER + T"
-          (mkLua "hl.dsp.exec_cmd(\"ghostty\")")
-        ];
-      }
+    mod = {
+      _var = "SUPER";
+    };
+    bind = lib.concatLists [
+      staticBinds
     ];
   };
 }
