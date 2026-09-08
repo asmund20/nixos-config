42a97fb3c44bde40c0afc823d991f6c1ae7adacd
Åsmund <asmund.sl@proton.me>
2026-09-08 11:52:32 +0200

    Finish grapple setup, remove unused autocommand

--- applications/home-manager/nvim/nvim/after/plugin/grapple.lua
+++ applications/home-manager/nvim/nvim/after/plugin/grapple.lua
@@ -1,6 +1,20 @@
 local grapple = require("grapple")
 
-vim.keymap.set("n", "<leader>a", grapple.toggle())
-vim.keymap.set("n", "<c-e>", grapple.toggle_loaded())
+grapple.setup({
+    icons = false,
+    status = false,
+})
 
-vim.keymap.set("n", "<c-1>", grapple.select(index=1))
+vim.keymap.set("n", "<leader>a", function() 
+    grapple.tag()
+end)
+
+vim.keymap.set("n", "<c-e>", function()
+    grapple.toggle_tags()
+end)
+
+for i=1,9,1 do
+    vim.keymap.set("n", "<c-" .. tostring(i%9) .. ">", function()
+        grapple.select({index=i})
+    end)
+end
--- applications/home-manager/nvim/nvim/lua/bakugan/autocommands.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function(args)
-        vim.wo.foldmethod = "expr"
-        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    end
-end,
-})
